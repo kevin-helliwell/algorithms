803df5168143585ce5c3e56a83ea29fec067d310
Kevin Helliwell <kbh789@gmail.com>
2023-03-11 21:09:39 -0500

    feat: implement bubble sort

---
+++ bubble_sort/src/main.rs
@@ -0,0 +1,21 @@
+// O(n^2) method for sorting arrays
+
+pub fn bubble_sort(array: &mut [usize]) -> () {
+    for i in 0..array.len() {
+        for j in 0..(array.len() - 1 - i) {
+            // if current element > next element, swap positions
+            if array[j] > array[j + 1] {
+                let larger_element = array[j]; // store larger element in variable for swap
+                let smaller_element = array[j + 1]; // store smaller element in variable for swap
+                array[j] = smaller_element; // smaller element -> j position
+                array[j + 1] = larger_element; // larger element -> j+1 position
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut test_array = [4, 5, 1, 2, 3];
+    bubble_sort(&mut test_array);
+    println!("{:?}", test_array); // "{:?}" to display arrays in println! (Debug trait)
+}
