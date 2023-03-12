e947ea4fc4e724ec0dc54b4519929e6cd4ceed63
Kevin Helliwell <kbh789@gmail.com>
2023-03-11 22:45:17 -0500

    feat: implement Stack data structure

---
+++ stack/src/main.rs
@@ -0,0 +1,36 @@
+pub struct Stack<T> {
+    stack: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Stack { stack: Vec::new() }
+    }
+
+    pub fn length(&self) -> usize {
+        self.stack.len()
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.stack.pop()
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.stack.push(item)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.stack.last()
+    }
+}
+
+fn main() {
+    let mut stack: Stack<isize> = Stack::new();
+    stack.push(100);
+    let item = stack.pop();
+    assert_eq!(item.unwrap(), 100);
+}
