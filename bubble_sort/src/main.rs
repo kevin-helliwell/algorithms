3cfe036d51651231f7626e98a147a9642644ffa3
Kevin Helliwell <kbh789@gmail.com>
2023-03-13 17:21:23 -0400

    fix: implement changes from cargo clippy

--- bubble_sort/src/main.rs
+++ bubble_sort/src/main.rs
@@ -1,6 +1,7 @@
 // O(n^2) method for sorting arrays
+// Remove () return suggested from cargo clippy: https://rust-lang.github.io/rust-clippy/master/index.html#unused_unit
 
-pub fn bubble_sort(array: &mut [usize]) -> () {
+pub fn bubble_sort(array: &mut [usize]) {
     for i in 0..array.len() {
         for j in 0..(array.len() - 1 - i) {
             // if current element > next element, swap positions
